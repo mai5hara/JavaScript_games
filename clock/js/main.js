b1d487f2d9bf1beb9948ef03bbf34235b18a4529
maigohara <maigohara-1103@outlook.jp>
2019-10-27 20:51:57 -0700

    add DrumKit foldar and Clock foldar.

---
+++ clock/js/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+{
+    const secondHand = document.querySelector('.second-hand');
+    const minHand = document.querySelector('.min-hand');
+    const hourHand = document.querySelector('.hour-hand');
+
+    function setDate() {
+        const now = new Date();
+        const seconds = now.getSeconds();
+        const secondsDegrees = ((seconds / 60) * 360) + 90;
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    
+        const mins = now.getMinutes();
+        const minsDegrees = ((mins / 60) * 360) + 90;
+        minHand.style.transform = `rotate(${minsDegrees}deg)`;
+    
+        const hours = now.getHours();
+        const hoursDegrees = ((hours / 12) * 360) + 90;
+        hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    
+    }
+
+    setInterval(setDate, 1000); //The setInterval() method will continue calling the function until clearInterval() is called, or the window is closed.
+
+
+
+
+
+
+
+
+}
